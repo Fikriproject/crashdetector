ca620d2a9e6ae089ba80177370345f1423544090
Fikri Project <120881342+Fikriproject@users.noreply.github.com>
2025-12-13 16:56:42 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.accident.detector',
+  appName: '.',
+  webDir: 'www'
+};
+
+export default config;
